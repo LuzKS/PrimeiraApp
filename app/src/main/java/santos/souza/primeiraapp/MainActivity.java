e1728a9f50472782a4e1b4ec25405e90daa9a36f
Lucas <lucas.esscontato@gmail.com>
2024-03-22 09:07:06 -0300

    modifiquei comentários

--- app/src/main/java/santos/souza/primeiraapp/MainActivity.java
+++ app/src/main/java/santos/souza/primeiraapp/MainActivity.java
@@ -13,31 +13,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //cria e busca o botão da interface xml
-        Button btnEnviar = findViewById(R.id.btnEnviar);
-
-        //ele vai "ouvir" o click do usuário
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main); //le arquivo layout e constroi interface
+        Button btnEnviar = findViewById(R.id.btnEnviar); //busca e obtém o botao da interface xml
+        btnEnviar.setOnClickListener(new View.OnClickListener() { //define o "ouvidor" de click do usuario
             @Override
-            //quando clicado, os comandos abaixo serão chamados
-            public void onClick(View v) {
-                //cria e busca o edittext da interface xml
-                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
-
-                //pega o texto digitado e guarda dentro da variável transformando ele em string
-                String textoDigitado = etDigiteAqui.getText().toString();
+            public void onClick(View v) {//quando clicado, os comandos abaixo serao chamados
+                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui); //busca e obtem o edittext da interface xml
+                String textoDigitado = etDigiteAqui.getText().toString(); //pega o texto digitado e guarda dentro da variavel transformando ele em string
 
                 //cria uma nova "intenção", ou seja, ele tem a intenção de que isso seja executado e será executado quando for possível
-                //a intenção é abrir a tela next actibity
+                //a intenção é abrir a tela next activity
                 Intent i = new Intent(MainActivity.this, NextActivity.class);
-
-                //ele cria uma chave e valor onde a chave ta entre aspas e o valor é o que ta dentro da variável texto digitado, manda td isso junto com a intenção
-                i.putExtra("amoeba", textoDigitado);
-
-                //inicia a nova tela sinalizada em Intente i
-                startActivity(i);
+                i.putExtra("amoeba", textoDigitado); //ele cria uma chave e valor onde a chave ta entre aspas e o valor é o que ta dentro da variável texto digitado, manda td isso junto com a intenção
+                startActivity(i); //navegar para a próxima tela sinalizada em Intent i
 
             }
         });
--- app/src/main/java/santos/souza/primeiraapp/NextActivity.java
+++ app/src/main/java/santos/souza/primeiraapp/NextActivity.java
@@ -12,18 +12,10 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-
-        //pega a intenção do main activity
-        Intent i = getIntent();
-
-        //pega a string criada em mainactivity
-        String amoeba = i.getStringExtra("amoeba");
-
-        //encontra o textview a partir do id
+        Intent i = getIntent(); //obtem o intent q criou essa tela
+        String amoeba = i.getStringExtra("amoeba"); //obtem o texto string vindo com a intent
         TextView tvTexto = findViewById(R.id.tvTexto);
-
-        //seta o texto digitado na string no textview
-        tvTexto.setText(amoeba);
+        tvTexto.setText(amoeba); //seta o texto digitado na string no textview
 
     }
 }
